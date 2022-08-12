010cbd7158c3e6a9fdab19d778c134ef7de2c7d7
tarok77 <96587138+tarok77@users.noreply.github.com>
2022-08-13 04:24:12 +0900

    add okhttp and change controller

--- src/main/java/com/tarok/quotegenerator/Controller/QuoteController.java
+++ src/main/java/com/tarok/quotegenerator/Controller/QuoteController.java
@@ -1,17 +1,25 @@
 package com.tarok.quotegenerator.Controller;
 
+import com.tarok.quotegenerator.Service.Okhttp;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class QuoteController {
+    private final Okhttp http;
+
+    public QuoteController(Okhttp http) {
+        this.http = http;
+    }
+
     @GetMapping("/")
     public String home() {
         return "home";
     }
     @PostMapping("/submit")
-    public String submit(@RequestParam("ISBN") String isbn) {
+    public String submit(@RequestParam("ISBN") String isbn) throws Exception {
         System.out.println(isbn);
+        http.run(isbn);
         return "redirect:/";
     }
 }
---
+++ src/main/java/com/tarok/quotegenerator/Service/Okhttp.java
@@ -0,0 +1,34 @@
+package com.tarok.quotegenerator.Service;
+
+import okhttp3.Headers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class Okhttp {
+    private final OkHttpClient client = new OkHttpClient();
+
+    public void run(String isbn) throws Exception {
+        String url = "https://www.googleapis.com/books/v1/volumes?q=isbn:"
+                + isbn;
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+
+            Headers responseHeaders = response.headers();
+            for (int i = 0; i < responseHeaders.size(); i++) {
+                System.out.println(responseHeaders.name(i) + ": " + responseHeaders.value(i));
+            }
+
+            System.out.println(response.body().string());
+        }
+    }
+}
