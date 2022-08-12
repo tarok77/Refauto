5534ec53e876288c473011184bdead195a13e7d1
tarok77 <96587138+tarok77@users.noreply.github.com>
2022-08-13 03:47:18 +0900

    add a method in Controller and so on

--- src/main/java/com/tarok/quotegenerator/Controller/QuoteController.java
+++ src/main/java/com/tarok/quotegenerator/Controller/QuoteController.java
@@ -1,13 +1,17 @@
 package com.tarok.quotegenerator.Controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class QuoteController {
     @GetMapping("/")
     public String home() {
         return "home";
+    }
+    @PostMapping("/submit")
+    public String submit(@RequestParam("ISBN") String isbn) {
+        System.out.println(isbn);
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/tarok/quotegenerator/Controller/Security/SecurityConfig.java
@@ -0,0 +1,57 @@
+package com.tarok.quotegenerator.Controller.Security;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http
+                    .formLogin(login -> login
+                            .loginProcessingUrl("/login")
+                            //.loginPage("/login")
+                            .usernameParameter("username")
+                            .passwordParameter("password")
+                            .defaultSuccessUrl("/",true)
+                            //.failureUrl("")
+                            .permitAll()
+
+                    ).logout(logout -> logout
+                            //要実装
+                            .logoutSuccessUrl("/")
+                    ).authorizeHttpRequests(authz -> authz
+                            .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
+                            .antMatchers("/h2-console/**").permitAll()
+                            .mvcMatchers("/","/login","/submit").permitAll()
+                            .mvcMatchers().hasRole("USER")
+                            .anyRequest().authenticated()
+                    ).headers().frameOptions().disable().and().cors().and().csrf().disable()//本番環境では取る。
+            ;
+            return http.build();
+        }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+        @Bean
+        public InMemoryUserDetailsManager userdetailsservice() {
+            PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            UserDetails user = User
+                    .withUsername("tes")
+                    .password(passwordEncoder.encode("pass"))
+                    .roles("USER")
+                    .build();
+            return new InMemoryUserDetailsManager(user);
+        }
+    }
