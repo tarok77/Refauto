1c0e16a7a36ce7719ff2faad12e428caa6a0de7d
tarok77 <96587138+tarok77@users.noreply.github.com>
2022-09-04 17:02:39 +0900

    add suppress warnings NonAsciiCharacters

--- src/test/java/com/tarok/quotegenerator/Repository/PublishedYearTest.java
+++ src/test/java/com/tarok/quotegenerator/Repository/PublishedYearTest.java
@@ -7,15 +7,16 @@ import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@SuppressWarnings("NonAsciiCharacters")
 class PublishedYearTest {
     @Test
     public void コンマ区切りの年月によるpublishedYearの生成が行われる() {
         var sut = new PublishedYear("2010.10");
-        assertThat(sut.getYearAndMonth()).isEqualTo(LocalDate.of(2010, 10, 01));
+        assertThat(sut.getYearAndMonth()).isEqualTo(LocalDate.of(2010, 10, 1));
         assertThat(sut.getYearAndMonth().getYear()).isEqualTo(2010);
         //日付が一文字の時のテスト
         var sut2 = new PublishedYear("1997.9");
-        assertThat(sut2.getYearAndMonth()).isEqualTo(LocalDate.of(1997, 9, 01));
+        assertThat(sut2.getYearAndMonth()).isEqualTo(LocalDate.of(1997, 9, 1));
         assertThat(sut2.getMonth()).isEqualTo(9);
     }
     @Test
