c5b0b6c4be19d42fa2b8f24a5dfeef2182886e58
tarok77 <96587138+tarok77@users.noreply.github.com>
2022-09-06 22:43:20 +0900

    delete LocalDate and add YearMonth

--- src/main/java/com/tarok/quotegenerator/Repository/ValueObjects/PublishedYear.java
+++ src/main/java/com/tarok/quotegenerator/Repository/ValueObjects/PublishedYear.java
@@ -3,22 +3,20 @@ package com.tarok.quotegenerator.Repository.ValueObjects;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 
-//出版年月を保存するためのクラス。Stringとして受けとったデータを整形しLocalDate型のフィールドとして保存する。LocalDate型を
-//使用したいためにダミーの日付を使用しているがマナーとして許されるのか。要検討。Month型を使って自分で実装するか。
+//出版年月を保存するためのクラス。Stringとして受けとったデータを整形しLocalDate型のフィールドとして保存する。YearMonthは継承不能なため委譲をつかう。
 //TODO　和暦がありうる。
 @Data
 public class PublishedYear {
     //同著者による同じ年度の本を並べ替え可能にするために念のため月も保存するようにしておく。
-    private final LocalDate YearAndMonth;
-
+    private final YearMonth YearAndMonth;
     public PublishedYear(String yearAndMonth) {
+        //一個にまとめる
         String tmp = yearAndMonth.replaceAll(" ", "").replaceAll("\\.","/");
-        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/M/dd");
-        LocalDate publishedYear = LocalDate.parse(tmp + "/01", dtf);
-
-        this.YearAndMonth = publishedYear;
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/M");
+        this.YearAndMonth = YearMonth.parse(tmp, dtf);
     }
 
     public int getYear() {
--- src/test/java/com/tarok/quotegenerator/Repository/PublishedYearTest.java
+++ src/test/java/com/tarok/quotegenerator/Repository/PublishedYearTest.java
@@ -4,6 +4,7 @@ import com.tarok.quotegenerator.Repository.ValueObjects.PublishedYear;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -12,11 +13,11 @@ class PublishedYearTest {
     @Test
     public void コンマ区切りの年月によるpublishedYearの生成が行われる() {
         var sut = new PublishedYear("2010.10");
-        assertThat(sut.getYearAndMonth()).isEqualTo(LocalDate.of(2010, 10, 1));
+        assertThat(sut.getYearAndMonth()).isEqualTo(YearMonth.of(2010, 10));
         assertThat(sut.getYearAndMonth().getYear()).isEqualTo(2010);
         //日付が一文字の時のテスト
         var sut2 = new PublishedYear("1997.9");
-        assertThat(sut2.getYearAndMonth()).isEqualTo(LocalDate.of(1997, 9, 1));
+        assertThat(sut2.getYearAndMonth()).isEqualTo(YearMonth.of(1997, 9));
         assertThat(sut2.getMonth()).isEqualTo(9);
     }
     @Test
