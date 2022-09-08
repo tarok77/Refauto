0322e1e89637245a0eca258dbea57743a9f7438d
tarok77 <96587138+tarok77@users.noreply.github.com>
2022-09-08 19:42:59 +0900

    add view and controller

---
+++ src/test/java/com/tarok/quotegenerator/Controller/BookForViewTest.java
@@ -0,0 +1,21 @@
+package com.tarok.quotegenerator.Controller;
+
+import com.tarok.quotegenerator.Repository.RawBook;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+
+class BookForViewTest {
+
+    @Test
+    void forViewによるrawBookStringリストのStringへの変更() {
+        var target = new RawBook();
+        target.setIsbn("11111");target.setCreatorList(List.of("test","test2"));target.setPublisher("publisher");
+        target.setTitle("testbook");target.setPublishedYear("1901");
+        var result = BookForView.toView(target);
+        assertThat(result.getCreators()).isEqualTo("test test2");
+    }
+}
