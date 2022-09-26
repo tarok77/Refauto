9a58f1e3c0301dd6a92e69e41824b1f8a32b2c5a
tarok77 <96587138+tarok77@users.noreply.github.com>
2022-09-26 18:11:09 +0900

    add javaScript and change views

--- src/main/resources/static/js/citedbook.js
+++ src/main/resources/static/js/citedbook.js
@@ -1,11 +1,6 @@
+document.getElementById("APA").style.display="block";
 document.getElementById("MLA").style.display="none";
 document.getElementById("Chicago").style.display="none";
-
-let copyText =
-    function(id) {
-        const text = document.getElementById(id);
-
-    }
 
 let changeDisplay =
     function(id) {
@@ -21,13 +16,29 @@ let changeDisplay =
 
         target.style.display="block";
     }
-let insertHidden =
-     function(id) {
-         let els = document.getElementById(id);
-          let fields = document.getElementById("hiddenField");
 
-          for (let i = 0; i < els.children.length; i++) {
-                fields.children[i].value = els.children[i].textContent;
-          }
-           document.formHidden.submit();
-       }
+//ユーザーに可視状態であるテクストを判別しコピー
+let copyText =
+    function() {
+        let APA = document.getElementById("APA");
+        let MLA = document.getElementById("MLA");
+        let chicago = document.getElementById("Chicago");
+
+        if(APA.style.display==="block") {
+            copy(APA.textContent);
+            return;
+        }
+        if(MLA.style.display==="block") {
+            copy(MLA.textContent);
+            return;
+        }
+        copy(Chicago.textContent);
+    }
+
+let copy =
+    function(text) {
+        navigator.clipboard.writeText(text)
+        .then(
+        (success) => alert("コピー完了。"),
+        (error) => alert("コピーに失敗しました。")
+        );}
